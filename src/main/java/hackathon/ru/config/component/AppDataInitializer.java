1553dbfd9380664eda85c92acac185533274036d
AndreevDan93 <andreevdan93@gmail.com>
2023-05-27 14:38:59 +0500

    add sql

--- src/main/java/hackathon/ru/config/component/AppDataInitializer.java
+++ src/main/java/hackathon/ru/config/component/AppDataInitializer.java
@@ -4,6 +4,7 @@ import hackathon.ru.data.service.CityService;
 import hackathon.ru.data.service.application.iService.ApplicationStatusService;
 import hackathon.ru.data.service.candidate.iservice.DegreeService;
 import hackathon.ru.data.service.user.iService.RoleService;
+import hackathon.ru.data.service.user.iService.UserService;
 import hackathon.ru.data.service.vacancy.iService.RequiredExperienceService;
 import hackathon.ru.data.service.vacancy.iService.VacancyStatusService;
 import hackathon.ru.data.service.vacancy.iService.WorkFormatService;
@@ -18,7 +19,7 @@ import javax.sql.DataSource;
 
 @Component
 @AllArgsConstructor
-public class Initializer {
+public class AppDataInitializer {
     private final DataSource dataSource;
     private final ApplicationStatusService applicationStatusService;
     private final CityService cityService;
@@ -27,51 +28,61 @@ public class Initializer {
     private final RoleService roleService;
     private final VacancyStatusService vacancyStatusService;
     private final WorkFormatService workFormatService;
+    private final UserService userService;
 
     @EventListener(ApplicationReadyEvent.class)
-    public void initAppData() {
+    public void initDictionaries() {
         if (vacancyStatusService.getAllVacancyStatuses().isEmpty()) {
             ResourceDatabasePopulator resourceDatabasePopulator
                     = new ResourceDatabasePopulator(false, false,
-                    "UTF-8", new ClassPathResource("sql/insert_vacancy_statuses.sql"));
+                    "UTF-8", new ClassPathResource("sql/dictionaries/insert_vacancy_statuses.sql"));
             resourceDatabasePopulator.execute(dataSource);
         }
         if (applicationStatusService.getAllApplicationStatuses().isEmpty()) {
             ResourceDatabasePopulator resourceDatabasePopulator
                     = new ResourceDatabasePopulator(false, false,
-                    "UTF-8", new ClassPathResource("sql/insert_application_status.sql"));
+                    "UTF-8", new ClassPathResource("sql/dictionaries/insert_application_status.sql"));
             resourceDatabasePopulator.execute(dataSource);
         }
         if (cityService.getAllCities().isEmpty()) {
             ResourceDatabasePopulator resourceDatabasePopulator
                     = new ResourceDatabasePopulator(false, false,
-                    "UTF-8", new ClassPathResource("sql/insert_city.sql"));
+                    "UTF-8", new ClassPathResource("sql/dictionaries/insert_city.sql"));
             resourceDatabasePopulator.execute(dataSource);
         }
         if (degreeService.getAllDegrees().isEmpty()) {
             ResourceDatabasePopulator resourceDatabasePopulator
                     = new ResourceDatabasePopulator(false, false,
-                    "UTF-8", new ClassPathResource("sql/insert_degree.sql"));
+                    "UTF-8", new ClassPathResource("sql/dictionaries/insert_degree.sql"));
             resourceDatabasePopulator.execute(dataSource);
         }
         if (requiredExperienceService.getAllRequiredExperiences().isEmpty()) {
             ResourceDatabasePopulator resourceDatabasePopulator
                     = new ResourceDatabasePopulator(false, false,
-                    "UTF-8", new ClassPathResource("sql/insert_required_experience.sql"));
+                    "UTF-8", new ClassPathResource("sql/dictionaries/insert_required_experience.sql"));
             resourceDatabasePopulator.execute(dataSource);
         }
         if (roleService.getAllRoles().isEmpty()) {
             ResourceDatabasePopulator resourceDatabasePopulator
                     = new ResourceDatabasePopulator(false, false,
-                    "UTF-8", new ClassPathResource("sql/insert_role.sql"));
+                    "UTF-8", new ClassPathResource("sql/dictionaries/insert_role.sql"));
             resourceDatabasePopulator.execute(dataSource);
         }
         if (workFormatService.getAllWorkFormats().isEmpty()) {
             ResourceDatabasePopulator resourceDatabasePopulator
                     = new ResourceDatabasePopulator(false, false,
-                    "UTF-8", new ClassPathResource("sql/insert_work_format.sql"));
+                    "UTF-8", new ClassPathResource("sql/dictionaries/insert_work_format.sql"));
             resourceDatabasePopulator.execute(dataSource);
         }
+
+        if (userService.getAllUsers().isEmpty()) {
+            ResourceDatabasePopulator resourceDatabasePopulator
+                    = new ResourceDatabasePopulator(false, false,
+                    "UTF-8", new ClassPathResource("sql/data/insert_user.sql"));
+            resourceDatabasePopulator.execute(dataSource);
+        }
+
     }
+
 }
 
---
+++ src/main/resources/sql/data/insert_user.sql
@@ -0,0 +1,7 @@
+INSERT INTO user (email, password, first_name, last_name, phone, role_id, city_id)
+VALUES ('andreevdan93@gmail.com', '$2a$12$AUdT9IEeMB/K3uH0uJbPxOZaODyCbZ3/T0AAeOM0JMbDnhHOSeXem', 'Даниил', 'Андреев', '80000000000', 1, 49),
+       ('xushaha@ya.ru', '$2a$12$AUdT9IEeMB/K3uH0uJbPxOZaODyCbZ3/T0AAeOM0JMbDnhHOSeXem', 'Ксения', 'Доможилова', '80000000000', 1, 57),
+       ('secondemail3413@gmail.com', '$2a$12$AUdT9IEeMB/K3uH0uJbPxOZaODyCbZ3/T0AAeOM0JMbDnhHOSeXem', 'Арсений', 'Прохоров', '8000000000', 1, 39),
+       ('a.g.samkin@gmail.com', '$2a$12$AUdT9IEeMB/K3uH0uJbPxOZaODyCbZ3/T0AAeOM0JMbDnhHOSeXem', 'Алексей', 'Прохоров', '80000000000', 1, 50),
+       ('Vadim.Tentuk', '$2a$12$AUdT9IEeMB/K3uH0uJbPxOZaODyCbZ3/T0AAeOM0JMbDnhHOSeXem', 'Вадим', 'Тентюк', '80000000000', 1, 11),
+       ('Sovcombank@sovkombank.ru', '$2a$12$AUdT9IEeMB/K3uH0uJbPxOZaODyCbZ3/T0AAeOM0JMbDnhHOSeXem', 'СовКомБанк', 'СовКомБанк', '80000000000', 1, 39);
